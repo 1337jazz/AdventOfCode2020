0df312fb38ebe8c6b45c36446f9a68b68ab7df69
Jazz Singh <jazz.singh@geshdo.com>
2020-12-02 10:51:35 +0100

    Day 2

---
+++ Solutions/Day2/Library/Policy.cs
@@ -0,0 +1,25 @@
+﻿namespace Solutions.Day2.Library
+{
+    public class Policy
+    {
+        public int Pos1 { get; private set; }
+        public int Pos2 { get; private set; }
+        public char Char { get; private set; }
+        public string Password { get; private set; }
+
+        public Policy(string line)
+        {
+            var split = line.Split(":");
+            var policyRaw = split[0];
+            var positions = policyRaw
+                .Substring(0, policyRaw.Length - 1)
+                .Trim()
+                .Split("-");
+
+            Password = split[1];
+            Pos1 = int.Parse(positions[0]);
+            Pos2 = int.Parse(positions[1]);
+            Char = policyRaw.Split(" ")[1][0];
+        }
+    }
+}
---
+++ Solutions/Program.cs
@@ -0,0 +1,14 @@
+﻿using Solutions.Day1;
+using Solutions.Day2;
+
+namespace Solutions
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Day1Solution.Run();
+            Day2Solution.Run();
+        }
+    }
+}
