c9239dbb2901b7aebf8231bc4cdb9fd655e14573
Jazz Singh <jazz.singh@geshdo.com>
2020-12-03 14:01:14 +0100

    Day 4 prep

---
+++ Solutions/Day4/Day4Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Solutions.Day4
+{
+    public class Day4Solution
+    {
+        private static void Run()
+        {
+            var data = File.ReadAllLines("Day4/puzzle_input.txt");
+            Console.WriteLine($"Day 4, part 1: {Part1(data)}");
+            Console.WriteLine($"Day 4, part 2: {Part2(data)}\n");
+        }
+
+        private static int Part1(string[] data)
+        {
+            return 0;
+        }
+
+        private static int Part2(string[] data)
+        {
+            return 0;
+        }
+    }
+}
